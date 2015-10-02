50c965da067db15ac8b5fce9e7f5378e86479a84
Christopher Williams <christopherwilliams@pcp080124pcs.wireless.calpoly.edu>
2015-10-02 13:34:29 -0700

    basic variables and constants

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let implicitStr = "I'm implicit!"
+let explicitStr: String = "I'm explicit!"
+
+var mutable = 20
+mutable = 30
+
+let label = "The answer is "
+let answer = 42
+let fullLabel = label + String(answer)
+
+let age = 21
+let interpolatedStr = " I am \(age) years old!"
+let interpolatedStr2 = " In 3 years I'll be \(age + 3) years old!"
+
