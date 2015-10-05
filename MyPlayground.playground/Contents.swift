c78e3c662d53f035f57bca179269ed170b4c8993
Christopher Williams <chrsw49@gmail.com>
2015-10-05 00:57:13 -0700

    more swift basics

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,17 +2,95 @@
 
 import UIKit
 
+// LEARN THE ESSENTIALS OF SWIFT
+
+// variable declaration
 let implicitStr = "I'm implicit!"
 let explicitStr: String = "I'm explicit!"
 
-var mutable = 20
-mutable = 30
-
+// type casting
 let label = "The answer is "
 let answer = 42
 let fullLabel = label + String(answer)
 
+// interpolated strings
 let age = 21
 let interpolatedStr = " I am \(age) years old!"
 let interpolatedStr2 = " In 3 years I'll be \(age + 3) years old!"
+
+// optional variables
+let stringNum = "banana"
+let possibleNum = Int(stringNum)
+print(possibleNum)
+
+// arrays
+var array = [5, 3, 4, 6]
+var emptyArray = [String]() // empty initializer
+array.append(17)
+for num in array {
+    print(num)
+}
+
+// switch-case
+let switchName = "test"
+switch switchName {
+case "test":
+    print("switch test")
+default:
+    print("switch default")
+}
+
+// for loop in range
+for i in 0..<4 {
+    print("this runs 4 times!")
+}
+
+// functions
+func sayName(name: String, _ iterations: Int, _ test: String) -> String {
+    var greeting = ""
+    let piece = "Hello \(name)! "
+    for _ in 0..<iterations {
+        greeting += piece
+    }
+    return test + greeting
+}
+
+sayName("Chris", 4, "Oh hey there, ") // subsequent parameters need to be identified (unless you add "_")
+
+// classes
+class Car {
+    var noise = ""
+    
+    init(_ noise: String) {
+        self.noise = noise
+    }
+    
+    func makeNoise() -> String {
+        return "\(noise) \(noise) MUTHAFUCKA"
+    }
+}
+
+var acura = Car("VROOM")
+acura.makeNoise()
+
+class Truck:Car {
+    var dickLen = 0
+    
+    init?(_ noise: String, _ dickLen: Int) {
+        super.init(noise)
+        if dickLen < 0 {
+            return nil
+        }
+        self.dickLen = dickLen
+    }
+    
+    override func makeNoise() -> String {
+        return "look at my \(dickLen) inch COCK, \(noise)"
+    }
+}
+
+var jackedUp = Truck("weeeee", 12)
+jackedUp?.makeNoise()
+var jackedDown = Truck("weeeee", -1)
+jackedDown?.makeNoise()
 
